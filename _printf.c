a7d6987e17f31909836c491f27b8480d5ecb1a6d
AlExito745 <alxtinoco1988@gmail.com>
2022-11-11 08:13:21 -0800

    add _printf.c file

---
+++ _printf.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+
+/**
+ * _printf - Format input to output
+ * @format: Input string.
+ *
+ * Return: The number of characters printed (excluding
+ * the null byte used to end output to strings)
+ **/
+
+int _printf(const char *format, ...)
+{
+	int result;
+	va_list argum;
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+
+	result = strlen(format);
+	if (result <= 0)
+	{
+		return (0);
+	}
+
+	va_start(argum, format);
+	result = controller(format, argum);
+
+	_putchar(-1);
+	va_end(argum);
+
+	return (result);
+}
