22e3575083b9789a07212ec95b7185054050c49f
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-11-11 09:42:43 -0800

    ADD:pstr.c file

---
+++ pstr.c
@@ -0,0 +1,24 @@
+#include "main.c"
+
+/**
+ * pstr - Print string
+ * @argum: list
+ *
+ * Return: String length.
+ */
+
+int pstr(va_list argum)
+{
+	char *s;
+
+	s = va_arg(argum, char*);
+	if (s != NULL)
+	{
+		return (p(s));
+	}
+	else
+	{
+		return (p("(NULL)"));
+	}
+
+}
