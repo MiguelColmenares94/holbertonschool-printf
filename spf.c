d6a2f9091aae9e251a97c4908e2527b72c04e1eb
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-11-11 09:42:13 -0800

    ADD:support files

---
+++ spf.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * p - print char
+ * @text: string to be printed
+ *
+ * Return: lenght of string
+ */
+
+int p(char *text)
+{
+	int a;
+
+	a = 0;
+	while (text[a] != '\0')
+	{
+		_putchar(text[a]);
+	++a;
+	}
+	return (a);
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
