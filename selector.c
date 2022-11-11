67d552f8395cb9a2a15f5ef01895f48f97e2def7
AlExito745 <alxtinoco1988@gmail.com>
2022-11-11 08:12:10 -0800

    add selector.c file

---
+++ selector.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * selector - Select format
+ * @text: Text to be formated
+ * @lf: Format list
+ * @a: Counter
+ *
+ * Return: Size of the numbers of elements printed
+ **/
+
+int selector(const char *text, va_list lf, int *a)
+{
+	int result, c, select_f;
+	fmt fmtl[] = {
+		{'s', print_string}, {'c', print_char}
+	};
+
+	*a = *a + 1;
+
+	if (text[*a] == '\0')
+		return (-1);
+
+	if (text[*a] == '%')
+	{
+		_putchar('%');
+		return (1);
+	}
+
+	select_f = sizeof(fmtl) / sizeof(fmtl[0]);
+	for (result = c = 0; c < select_f; c++)
+	{
+		if (text[*a] == fmtl[c].type)
+		{
+			result = fmtl[c].f(lf);
+			return (result);
+		}
+
+	}
+
+	_putchar('%'), _putchar(text[*a]);
+
+	return (2);
+}
