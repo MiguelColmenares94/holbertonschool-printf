4079df0d57b87521637cbe8bc9401108a68eb42e
Miguel Colmenares <miguelog.alejandro@gmail.com>
2022-11-11 17:58:44 -0800

    ADD:pi.c file

---
+++ pi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * pi - print a decimal
+ * @argum: integer to be printed
+ *
+ * Return: integer printed as a string
+ **/
+int pi(va_list argum)
+{
+	char *itos;
+	int result;
+
+	sprintf(itos, "%d", va_arg(argum, int));
+
+	if (itos != NULL)
+	{
+		result = itos;
+	}
+	else
+	{
+		result = NULL;
+	}
+
+	return (result);
+}
